d7bc749f27a947d2baab77d837799345f1de5e72
Laurent Goderre <laurent.goderre@gmail.com>
2014-03-05 17:45:24 -0500

    Added a build artifact script

---
+++ scripts/travis_artifacts.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+start=$(date +%s)
+echo -e "Current repo: $TRAVIS_REPO_SLUG\n"
+
+function error_exit
+{
+	echo -e "\e[01;31m$1\e[00m" 1>&2
+	exit 1
+}
+
+if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_REPO_SLUG" == "LaurentGoderre/project" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+
+	#Set git user
+	git config --global user.email "ninthlegionmilsim@gmail.com"
+	git config --global user.name "9th Legion Milsim"
+
+	#Add the latest build result
+	echo -e "Uploading the build artifacts\n"
+
+	grunt deploy || error_exit "Error running gh-pages task";
+fi
+
+end=$(date +%s)
+elapsed=$(( $end - $start ))
+minutes=$(( $elapsed / 60 ))
+seconds=$(( $elapsed % 60 ))
+echo "Post-Build process finished in $minutes minute(s) and $seconds seconds"
